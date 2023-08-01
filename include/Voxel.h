7ac0cf1834b6dd9ad5673df47a31f6b509ba7845
Albin Kjellson <albinkj91@yahoo.com>
2023-08-01 19:37:05 +0200

    Removed unused constants

--- include/Voxel.h
+++ include/Voxel.h
@@ -13,11 +13,6 @@ public:
 
 	glm::vec3 get_position();
 	virtual void render() const = 0;
-
-	static const unsigned int GRASS = 0;
-	static const unsigned int DIRT = 1;
-	static const unsigned int FLOWER = 2;
-	static const unsigned int TREE = 3;
 private:
 	glm::vec3 position;
 };
